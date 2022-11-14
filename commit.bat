59b2a73e6f81d836c23fd706b50096b11466b8bc
Samet Kula <sametkula@proton.me>
2022-11-14 23:02:57 +0300

    -14.11.2022-_'s upload

--- commit.bat
+++ commit.bat
@@ -1,4 +1,5 @@
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 echo %mydate%_%mytime% upload's commit >> sss.txt
 git add .
 git commit -m "%mydate%_%mytime%'s upload"
